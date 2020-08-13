a78abb681aa78c804d9fa45aa162b0bc9c514454
tommiport5 <tommi.port5.gmail.com>
2020-08-13 13:38:42 +0200

    RolandPatch.readMemory tested successfully

--- rolandpatch.js
+++ rolandpatch.js
@@ -23,14 +23,31 @@ const FillState = Object.freeze ({
 	lpc: 6,
 	pd: 7
 });
- 
+
+function delay(ms) {
+	return new Promise((resolve) => {
+		setTimeout(resolve(), ms);
+	});
+}
+
+function address2PatchAndState(adr) {
+	let point = (adr - 32768) / 448;
+	let patch = Math.trunc(point);
+	let state = Math.round((point-patch)*7) + 1;
+	return [patch, state];
+}
+
 module.exports = class RolandPatch {
 	constructor(MIn, MOut, MChan) {
 		this.mIn = MIn;
 		this.mOut = MOut;
 		this.mChan = MChan;
+		this._complete = false;
 	}
 	
+	get complete() {
+		return this.complete;
+	}
 	
 	static toStr(arr) {
 		var res = "";
@@ -43,11 +60,14 @@ module.exports = class RolandPatch {
 	}
 	
 	get patchname() {
+		var res;
 		if (this.pd == undefined) {
-			return "<undefined>";
+			res = "<undefined>";
 		} else {
-			return RolandPatch.toStr(this.pd.slice(0,18));
+			res = RolandPatch.toStr(this.pd.slice(0,18));
+			if (!this._complete) res += " <incomplete>";
 		}
+		return res;
 	}
 	
 	get tonenames() {
@@ -63,15 +83,15 @@ module.exports = class RolandPatch {
 	}
 	
 	static threebyte2num(arr) {
-		return 0x80*(0x80*arr[0] + arr[1]) + arr[2];
-	}
+		return ((arr[0] & 0x7f) << 14) | ((arr[1] & 0x7f) << 7) | (arr[2] & 0x7f);
+	}		
 			
 	static num2threebyte(num) {
 		var res = [0,0,0];
 		var n = Math.round(num);
-		res[2] = num % 0x80;
-		res[1] = Math.trunc(n/0x80 % 0x80);
-		res[0] = Math.trunc(n/(0x80*0x80) % 0x80);
+		res[2] = n & 0x7f;
+		res[1] = (n >> 7) & 0x7f;
+		res[0] = (n >> 14) & 0x7f;
 		return res;
 	}
 	
@@ -127,6 +147,7 @@ module.exports = class RolandPatch {
 				}).then((sx) =>{
 					console.log("received command: " + sx.command);
 					ack.send(This.mOut);
+					this._complete = true;
 					resolve("ok");
 					return Ret;
 				}).catch((tmo) => {
@@ -150,44 +171,80 @@ module.exports = class RolandPatch {
 			});
 		});
 	}
-	
-	static _fillResult(raw, accu) {
+
+	static _fillResult(rw, accu) {
+		if (accu.pnum >= 64) return false;		// ignore reverb for now
+		let threeb = rw.slice(0,3);
+		let found = address2PatchAndState(RolandPatch.threebyte2num(threeb));
+		let raw = rw.slice(3);
+		if ((found[0] != accu.pnum) || (found[1] != accu.fs)){
+			console.log(`resync to ${found[0]}, ${found[1]} instead of ${accu.pnum}, ${accu.fs}, addr ${threeb}`);
+			accu.pnum = found[0];
+			accu.fs = found[1];
+		}
+		if (accu.overflow.length > 0) {
+			//console.log(`Overflow ${accu.overflow.length} after patch ${accu.Result.length}, state ${accu.fs}`);
+			raw = accu.overflow.concat(raw);
+			accu.overflow = [];
+		}
 		do {
+			if (accu.pnum >= 64) return false;		// ignore reverb for now
+			if (accu.Result[accu.pnum] == undefined) accu.Result[accu.pnum] = new RolandPatch;
 			switch (accu.fs) {
 				case FillState.up1:
-					if (raw.length == 0) return;
-					if (accu.Result.length >= 64) return;		// ignore reverb for now
-					accu.Current.upd1 = raw.splice(0,64);
+					if (raw.length < 64) {
+						accu.overflow = raw;
+						return true;
+					}
+					accu.Result[accu.pnum].upd1 = raw.splice(0,64);
 					accu.fs = FillState.up2;
 				case FillState.up2:
-					if (raw.length == 0) return;
-					accu.Current.up2 = raw.splice(0,64);
+					if (raw.length < 64) {
+						accu.overflow = raw;
+						return true;
+					}
+					accu.Result[accu.pnum].up2 = raw.splice(0,64);
 					accu.fs = FillState.upc;
 				case FillState.upc:
-					if (raw.length == 0) return;
-					accu.Current.upc = raw.splice(0,64);
+					if (raw.length < 64) {
+						accu.overflow = raw;
+						return true;
+					}
+					accu.Result[accu.pnum].upc = raw.splice(0,64);
 					accu.fs = FillState.lp1;
 				case FillState.lp1:
-					if (raw.length == 0) return;
-					accu.Current.lp1 = raw.splice(0,64);
+					if (raw.length < 64) {
+						accu.overflow = raw;
+						return true;
+					}
+					accu.Result[accu.pnum].lp1 = raw.splice(0,64);
 					accu.fs = FillState.lp2;
 				case FillState.lp2:
-					if (raw.length == 0) return;
-					accu.Current.lp2 = raw.splice(0,64);
+					if (raw.length < 64) {
+						accu.overflow = raw;
+						return true;
+					}
+					accu.Result[accu.pnum].lp2 = raw.splice(0,64);
 					accu.fs = FillState.lpc;
 				case FillState.lpc:
-					if (raw.length == 0) return;
-					accu.Current.lpc = raw.splice(0,64);
+					if (raw.length < 64) {
+						accu.overflow = raw;
+						return true;
+					}
+					accu.Result[accu.pnum].lpc = raw.splice(0,64);
 					accu.fs = FillState.pd;
 				case FillState.pd:
-					if (raw.length == 0) return;
-					accu.Current.pd = raw.splice(0,64);
-					accu.Result.push(accu.Current);
-					accu.Current = new RolandPatch;
+					if (raw.length < 64) {
+						accu.overflow = raw;
+						return true;
+					}
+					accu.Result[accu.pnum].pd = raw.splice(0,64);
+					accu.Result[accu.pnum]._complete = true;
+					accu.pnum++;
 					accu.fs = FillState.up1;
 			}
 		} while (raw.length > 0);
-	};
+	}
 
 	/**
 	 * readMemory
@@ -196,14 +253,16 @@ module.exports = class RolandPatch {
 	 * The D50 must initiate the transfer with a WSD telegram, which is awaited in waitForWSD.
 	 */
 	static async readMemory(mIn, mOut, mChan) {
-		var Accu = {Result: [], 
+		var Accu = {Result: new Array(64), 
 			fs: FillState.up1,
+			pnum: 0,
 			Current: new RolandPatch,
+			overflow: [], 
+			expected: 32768
 		};
 		
 		try {
 			var resp;
-			var rd = new Sysex();
 			var ds = new Sysex();
 			var ack = new Sysex();
 			ack.brand = 0x41;
@@ -211,13 +270,7 @@ module.exports = class RolandPatch {
 			ack.model = 0x14;
 			ack.command = _ACK;
 			var Ret = ds.listen(mIn);
-			rd.brand = 0x41;
-			rd.channel = mChan;
-			rd.model = 0x14;
-			rd.command = _RQD;
-			rd.append([2,0,0]);
-			rd.append(RolandPatch.num2threebyte(64*448));
-			console.log("sending >>>" + rd.sendData + "<<");
+			// console.log("sending >>>" + rd.sendData + "<<");
 			ack.send(mOut);
 			do {
 				let sx = await Ret;
@@ -225,12 +278,14 @@ module.exports = class RolandPatch {
 				//console.log(`response 0x${resp.toString(16)}`);
 				if (resp == _DAT) {
 					Ret = ds.listen(mIn);
-					//console.log(`received ${sx.raw.length-3} byte`); 
+					let rcv = sx.raw.length-3;
+					if (rcv < 256) console.log(`received ${rcv} byte`); 
 					// fill a RolandPatch with the data
-					RolandPatch._fillResult(sx.raw.slice(3), Accu);
+					RolandPatch._fillResult(sx.raw, Accu);
+					//await delay(20);
 					ack.send(mOut);
 				} else if (resp != _EOD) {
-					console.log("protocol error in readMemory");
+					console.log("protocol error, received 0x${resp.toString(16)} instead of 0x40 or 0x45 in readMemory");
 					throw "protocol error";
 				}
 			} while(resp != _EOD);
@@ -242,3 +297,4 @@ module.exports = class RolandPatch {
 		}			
 	}
 }
+
